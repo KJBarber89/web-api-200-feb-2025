b858330c52d69f71882d6c0f0fb29ac152205462
Kelsey <kjbarber89@gmail.com>
2025-02-24 20:01:05 +0000

    updated changes w/ jeffs code- tests work

---
+++ src/issues/IssueTrackerSolution/IssueTracker.Api/Employee/Domain/EmployeeRepository.cs
@@ -0,0 +1,43 @@
+﻿using Marten;
+
+namespace IssueTracker.Api.Employee.Domain;
+
+public class EmployeeRepository(IDocumentSession session)
+{
+    public Employee Create(string sub)
+    {
+        var employeeEntity = new EmployeeEntity
+        {
+            Id = Guid.NewGuid(),
+            Sub = sub
+        };
+        return new Employee(employeeEntity, this);
+    }
+
+    internal async Task SaveAsync(EmployeeEntity entity, CancellationToken token)
+    {
+        // we'll talk about the entity in a second.
+        await session.SaveChangesAsync(token);
+    }
+}
+
+// our "service" from last class.
+
+public class Employee(EmployeeEntity entity, EmployeeRepository repository)
+{
+    public Guid Id { get; } = entity.Id;
+
+    public async Task SaveAsync(CancellationToken token)
+    {
+        await repository.SaveAsync(entity, token);
+    }
+   
+}
+// Our "persistence model" - what we are saving, etc.
+public class EmployeeEntity
+{
+    public Guid Id { get; set; }
+    // Network Id, verified identity, etc.
+    public string Sub { get; set; } = string.Empty;
+    
+}
--- src/issues/IssueTrackerSolution/IssueTracker.Tests/Employee/Domain/EmployeeTests.cs
+++ src/issues/IssueTrackerSolution/IssueTracker.Tests/Employee/Domain/EmployeeTests.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using IssueTracker.Api.Employee.Domain;
+using IssueTracker.Tests.Fixtures;
+using Marten;
 
-namespace IssueTracker.Tests.Employee.Domain
+namespace IssueTracker.Tests.Employee.Domain;
+
+[Trait("Category", "UnitIntegration")] // this is metadata so we can run just some of these at a time.
+[Collection("UnitIntegration")] // everything here should use the same database as everything else in this "collection"
+public class EmployeeTests(UnitIntegrationTestFixture fixture)
 {
-    class EmployeeTests
+    [Fact]
+
+    public void CanCreateAnEmployee()
     {
+        IDocumentSession session = fixture.Store.LightweightSession(); 
+
+
+        var repository = new EmployeeRepository(session); // a thing that handles persistence.
+        var sub = "bob@company";
+        var emp = repository.Create(sub);
+
+        
+
+
+
+
+    }
+
+    [Fact]
+    public void EmployeeCanSubmitProblems()
+    {
+
     }
 }
