f38cad18d72aa40ee813de7f0e37efe61f2eea84
Kelsey <kjbarber89@gmail.com>
2025-02-25 21:48:13 +0000

    updated file?

--- src/issues/IssueTrackerSolution/IssueTracker.Tests/Employee/Domain/CurrentEmployeeCommandProcessorTests.cs
+++ src/issues/IssueTrackerSolution/IssueTracker.Tests/Employee/Domain/CurrentEmployeeCommandProcessorTests.cs
@@ -1,9 +1,22 @@
 ﻿
+using IssueTracker.Api.Employees.Api;
+using IssueTracker.Api.Employees.Domain;
+using Microsoft.AspNetCore.Http;
 
-namespace IssueTracker.Tests.Employee.Domain;
-    [Trait("Category", "UnitIntegration")]
+namespace IssueTracker.Api.Employees.Services;
+
+public class CurrentEmployeeCommandProcessor(IHttpContextAccessor context) : IProcessCommandsForTheCurrentEmployee
 {
-    class CurrentEmployeeCommandProcessorTests
+    public Task<ProblemSubmitted> ProcessProblemAsync(SubmitProblem problem)
     {
+        var sub = context?.HttpContext?.User.Identity?.Name ?? throw new Exception("This should never happen");
+
+        // but we need the employee to do this,
+        // we need the sub claim
+        // look it up in the database, if isn't there, create it
+        // if it is there, load, tell it process this command, 
+        // save it. 
+        // return the ProblemSubmitted.
+        throw new Exception();
     }
 }
